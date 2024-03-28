a250781b6050e89118d695796a222741f5c7a5cc
Alvaro Rubina <alvarorubina132@gmail.com>
2024-03-28 13:30:24 -0300

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.example.clases.Minimarket;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +8,7 @@ public class Main {
         // Instancia del minimarket
         Minimarket minimarket = new Minimarket();
 
-        // Menu
+        // Menu de seleccion
         System.out.println("-- MINIMARKET --");
         minimarket.menuDeSeleccion();
 
