039748ffb5ae177f44a308013b0569c25c7b4a4b
Alvaro Rubina <alvarorubina132@gmail.com>
2024-03-28 15:17:02 -0300

    Agregados atributos de Factura

--- src/main/java/org/example/clases/Factura.java
+++ src/main/java/org/example/clases/Factura.java
@@ -1,4 +1,11 @@
 package org.example.clases;
 
+import java.time.LocalDate;
+
 public class Factura {
+
+    // Atributos
+    private int id;
+    private LocalDate fechaEmision;
+    private double monto;
 }
