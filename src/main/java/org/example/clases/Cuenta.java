6d4f15ad806dbffaf571365f31d04dfd1d971b3e
Alvaro Rubina <alvarorubina132@gmail.com>
2024-03-28 15:16:54 -0300

    Agregados atributos de Cuenta

--- src/main/java/org/example/clases/Cuenta.java
+++ src/main/java/org/example/clases/Cuenta.java
@@ -1,4 +1,11 @@
 package org.example.clases;
 
+import java.time.LocalDate;
+
 public class Cuenta {
+
+    // Atributos
+    private LocalDate fechaEmision;
+    private double monto;
+    private double propina;
 }
