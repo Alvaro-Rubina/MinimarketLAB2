d7f2c36836623664bd01e5fac9e0cdf13d18c3d2
Alvaro Rubina <alvarorubina132@gmail.com>
2024-03-29 12:03:45 -0300

    update minimarket

--- src/main/java/org/example/clases/Minimarket.java
+++ src/main/java/org/example/clases/Minimarket.java
@@ -348,7 +348,7 @@ public class Minimarket {
             }
         }
     }
-    
+
     // --------------------------------------------------------
     private double verificarIngresoValorNumerico(double eleccion){
 
